4ba86d6b9ad0d56e606b97b22a7d061f49d2f233
SA-design-1 <cathy@seoulauction.com>
2026-04-24 10:02:51 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+
+supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+const SUPABASE_ANON_KEY = "sb_publishable_9p50RVtpPdmZOG2emGTDVg_NQ3bp8U8";
+const SUPABASE_URL = "https://iznnctfnmeiqdjljounq.supabase.co";
